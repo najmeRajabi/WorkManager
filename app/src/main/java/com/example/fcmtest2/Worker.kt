e03b124da84d46fc600f929e9856c94c0ace7b37
niha8515 <niha8515@yahoo.com>
2022-06-25 01:10:30 +0430

    feat work manager and notification

--- app/src/main/java/com/example/fcmtest2/Worker.kt
+++ app/src/main/java/com/example/fcmtest2/Worker.kt
@@ -1,18 +1,56 @@
 package com.example.fcmtest2
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkRequest
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 
-class UploadWorker(appContext: Context, workerParams: WorkerParameters):
+class UploadWorker(private val appContext: Context, workerParams: WorkerParameters):
     Worker(appContext, workerParams) {
     override fun doWork(): Result {
-
-        // Do the work here--in this case, upload the images.
-//        uploadImages()
-
-        // Indicate whether the work finished successfully with the Result
+        createNotificationChannel()
         return Result.success()
+    }
+
+    private fun createNotificationChannel() {
+
+        //channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel_name"
+            val descriptionText = "channel_description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("id", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+        var builder = NotificationCompat.Builder(appContext, "id")
+            .setSmallIcon(R.drawable.ic_baseline_beach_access_24)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                .bigText("Much longer text that cannot fit one line..."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(appContext)) {
+            // notificationId is a unique int for each notification that you must define
+            val notificationId = 0
+            notify(notificationId, builder.build())
+        }
+
+
+
+
     }
 }
